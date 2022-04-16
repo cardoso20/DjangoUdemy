ebb8245fb0c19290232af33e2c25ffee45fb073e
Guilherme Cardoso <guicar97@hotmail.com>
2022-04-16 16:29:34 +0100

    header

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html') #global/home.html chama o template base
-
-def contact(request):
-    return HttpResponse('contact')
-
-def about(request):
-    return HttpResponse('about')
+    return render(request, 'recipes/pages/home.html') #global/home.html chama o template base
