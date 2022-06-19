76598e6310c8f28f3da24c0c3f4478138671e621
Guilherme Cardoso <guicar97@hotmail.com>
2022-06-19 01:19:50 +0100

    staticfiles

--- DjangoUdemy/settings.py
+++ DjangoUdemy/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/' #so de aparencia do url dos static files na source do ficheiro (ver F12 no browser)
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static'
+]
+STATIC_ROOT = BASE_DIR / 'static' #utilizado para guardar todos os static files do projeto nesta pasta (util para passagem a prod)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
