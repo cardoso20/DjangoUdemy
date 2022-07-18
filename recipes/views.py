4c481c41b36ba245e911f5042c11e3a0de6a9027
Guilherme Cardoso <guicar97@hotmail.com>
2022-07-19 00:08:21 +0100

    views e html css

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,14 @@
+from tkinter.tix import Tree
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html') #global/home.html chama o template base
+    return render(request, 'recipes/pages/home.html', context= {
+        'recipes': [make_recipe() for _ in range(10)]
+    }) #global/home.html chama o template base
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe.html', context= {
+        'recipe': make_recipe(),
+        'is_detail_page': True
+    })
