e8d192798c6dca305480e0577fb38ed5ed13e41e
Guilherme Cardoso <guicar97@hotmail.com>
2022-07-19 02:24:49 +0100

    models

--- DjangoUdemy/settings.py
+++ DjangoUdemy/settings.py
@@ -125,6 +125,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / 'static' #utilizado para guardar todos os static files do projeto nesta pasta (util para passagem a prod)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
+from re import A
 from django.contrib import admin
+from models import Recipe, Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Category, CategoryAdmin)
